c1437d3e36d38b1489c201eb5b13d50971f54798
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-16 22:52:30 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+lass Backer
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+
+  end
+
+end
